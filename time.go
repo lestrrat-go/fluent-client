ebf4f753dc8590430e2c06e1497191834a0bba35
Daisuke Maki <lestrrat+github@gmail.com>
2017-05-18 07:41:21 +0900

    Fix to match new msgpack ext decode/encode

--- time.go
+++ time.go
@@ -14,32 +14,7 @@ func init() {
 }
 
 func (t *EventTime) DecodeMsgpack(d *msgpack.Decoder) error {
-	code, err := d.ReadCode()
-	if err != nil {
-		return errors.Wrap(err, `failed to read code`)
-	}
-
 	r := d.Reader()
-	switch code {
-	case msgpack.FixExt8:
-		// no op. we know we have 8 bytes
-	case msgpack.Ext8:
-		size, err := r.ReadUint8()
-		if err != nil {
-			return errors.Wrap(err, `failed to read ext8 length`)
-		}
-
-		if size != 8 {
-			return errors.Wrap(err, `expected 8 bytes payload`)
-		}
-	default:
-		return errors.Errorf(`invalid code %s`, code)
-	}
-
-	typ, err := r.ReadUint8()
-	if typ != 0 {
-		return errors.Errorf(`invalid ext type %d`, typ)
-	}
 
 	sec, err := r.ReadUint32()
 	if err != nil {
@@ -56,9 +31,6 @@ func (t *EventTime) DecodeMsgpack(d *msgpack.Decoder) error {
 }
 
 func (t EventTime) EncodeMsgpack(e *msgpack.Encoder) error {
-	e.EncodeExtHeader(8)
-	e.EncodeExtType(t)
-
 	w := e.Writer()
 	if err := w.WriteUint32(uint32(t.Unix())); err != nil {
 		return errors.Wrap(err, `failed to write EventTime seconds payload`)
